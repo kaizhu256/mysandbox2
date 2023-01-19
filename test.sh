bec786d880098fce6239276516e25237dfdee7b7
kai zhu <kaizhu256@gmail.com>
2023-01-19 03:39:09 -0600

    tweak

--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ echo $PWD
 #!! powershell "Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'"
 powershell 'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0'
 
+powershell 'netsh advfirewall firewall add rule name="Open Port 22" dir=in action=allow protocol=TCP localport=22'
+
+
 echo aa
 ssh runneradmin@localhost dir
 echo bb
