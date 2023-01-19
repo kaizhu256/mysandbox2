b90d13f365d62ee056c4be245c9e46160ce64b8d
kai zhu <kaizhu256@gmail.com>
2023-01-19 05:23:57 -0600

    tweak

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ AuthorizedKeysFile	.ssh/authorized_keys
 cat /c/programdata/ssh/sshd_config
 powershell 'Start-Service sshd'
 
-cd .ssh
+cd ~/.ssh
 ssh-keygen -C "your_email@example.com" -N "" -f ~/.ssh/id_ed25519 -t ed25519
 cp id_ed25519.pub authorized_keys
 #!! powershell 'netsh advfirewall firewall add rule name="Open Port 22" dir=in action=allow protocol=TCP localport=22'
