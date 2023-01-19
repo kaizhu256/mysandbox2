02615956b64176c9a7a38c62742de63f470018c9
kai zhu <kaizhu256@gmail.com>
2023-01-19 03:35:14 -0600

    tweak

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@ uname
 ls -la
 echo $PWD
 
-powershell "Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'"
-powershell 'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0'
+#!! powershell "Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'"
+#!! powershell 'Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0'
 
 echo aa
 ssh runneradmin@localhost dir
